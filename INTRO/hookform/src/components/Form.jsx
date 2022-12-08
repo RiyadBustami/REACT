984d1cdde3454eaf56d5c7327f4157ba4a49e8af
Riyad Busttami <Riyad.Bustami@gmail.com>
2022-12-08 15:24:11 +0200

    More Forms Assignment done

--- INTRO/hookform/src/components/Form.jsx
+++ INTRO/hookform/src/components/Form.jsx
@@ -6,29 +6,94 @@ const Form = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConf, setPasswordConf] = useState("");
+    const [firstNameError, setFirstNameError] = useState("");
+    const [lastNameError, setLastNameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confPasswordError, setConfPasswordError] = useState("");
+
+    const fNameHandle = (e)=>{
+        if(e.target.value.length<2&&e.target.value.length!=0){
+            setFirstNameError("First Name must be at least 2 characters");
+        }
+        else{
+            setFirstNameError("");
+            setFirstName(e.target.value);
+        }
+    }
+    const lNameHandle = (e)=>{
+        if(e.target.value.length<2&&e.target.value.length!=0){
+            setLastNameError("Last Name must be at least 2 characters");
+        }
+        else{
+            setLastNameError("");
+            setLastName(e.target.value);
+        }
+    }
+    const emailHandle = (e)=>{
+        if(e.target.value.length<2&&e.target.value.length!=0){
+            setEmailError("Email must be at least 2 characters");
+        }
+        else{
+            setEmailError("");
+            setEmail(e.target.value);
+        }
+    }
+    const passwordHandle = (e)=>{
+        if(e.target.value.length<8&&e.target.value.length!=0){
+            setPasswordError("Password must be at least 8 characters");
+        }
+        else{
+            setPasswordError("");
+            setPassword(e.target.value);
+        }
+    }
+    const confirmPasswordHandle = (e)=>{
+        if(e.target.value!=password&&e.target.value.length!=0){
+            setConfPasswordError("Confirm Password must match password");
+        }
+        else{
+            setConfPasswordError("");
+            setPasswordConf(e.target.value);
+        }
+    }
+
 
     return (
         <div>
             <form>
                 <div>
                     <label htmlFor={"firstName"}>First Name: </label>
-                    <input type={"text"} onChange={(e) => setFirstName(e.target.value)} value={firstName}></input>
+                    <input type={"text"} onChange={fNameHandle}></input>
+                    <div>
+                        <p style={{color:"red"}}>{firstNameError}</p>
+                    </div>
                 </div>
                 <div>
                     <label htmlFor={"lastName"}>Last Name: </label>
-                    <input type={"text"} onChange={(e) => setLastName(e.target.value)} value={lastName}></input>
+                    <input type={"text"} onChange={lNameHandle}></input>
+                    <div>
+                        <p style={{color:"red"}}>{lastNameError}</p>
+                    </div>
                 </div>
                 <div>
                     <label htmlFor={"email"}>Email: </label>
-                    <input type={"email"} onChange={(e) => setEmail(e.target.value)} value={email}></input>
+                    <input type={"email"} onChange={emailHandle}></input>
+                    <div>
+                        <p style={{color:"red"}}>{emailError}</p>
+                    </div>
                 </div>
                 <div>
                     <label htmlFor={"password"}>Password: </label>
-                    <input name={'password'} type={"password"} onChange={(e) => setPassword(e.target.value)} value={password}></input>
+                    <input name={'password'} type={"password"} onChange={passwordHandle}></input>
+                    <div>
+                        <p style={{color:"red"}}>{passwordError}</p>
+                        <p style={{color:"red"}}>{confPasswordError}</p>
+                    </div>
                 </div>
                 <div>
                     <label htmlFor={"passwordconf"}>Confirm Password: </label>
-                    <input name={'passwordconf'} type={"password"} onChange={(e) => setPasswordConf(e.target.value)} value={passwordConf}></input>
+                    <input name={'passwordconf'} type={"password"} onChange={confirmPasswordHandle}></input>
                 </div>
             </form>
             <hr></hr>
