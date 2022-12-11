514924abc9fdd0c05b87e0379f1406cbab782159
Riyad Busttami <Riyad.Bustami@gmail.com>
2022-12-11 16:21:06 +0200

    after code review add city

--- INTRO/hookform/src/components/Form.jsx
+++ INTRO/hookform/src/components/Form.jsx
@@ -6,53 +6,56 @@ const Form = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConf, setPasswordConf] = useState("");
+    const [city, setCity] = useState("");
     const [firstNameError, setFirstNameError] = useState("");
     const [lastNameError, setLastNameError] = useState("");
     const [emailError, setEmailError] = useState("");
     const [passwordError, setPasswordError] = useState("");
     const [confPasswordError, setConfPasswordError] = useState("");
+    // const [cityError, setCityError] =useState("");
+    const cities = ["Nablus", "Ramallah", "Qalqilya", "Hebron"]
 
-    const fNameHandle = (e)=>{
-        if(e.target.value.length<2&&e.target.value.length!=0){
+    const fNameHandle = (e) => {
+        if (e.target.value.length < 2 && e.target.value.length != 0) {
             setFirstNameError("First Name must be at least 2 characters");
         }
-        else{
+        else {
             setFirstNameError("");
             setFirstName(e.target.value);
         }
     }
-    const lNameHandle = (e)=>{
-        if(e.target.value.length<2&&e.target.value.length!=0){
+    const lNameHandle = (e) => {
+        if (e.target.value.length < 2 && e.target.value.length != 0) {
             setLastNameError("Last Name must be at least 2 characters");
         }
-        else{
+        else {
             setLastNameError("");
             setLastName(e.target.value);
         }
     }
-    const emailHandle = (e)=>{
-        if(e.target.value.length<2&&e.target.value.length!=0){
+    const emailHandle = (e) => {
+        if (e.target.value.length < 2 && e.target.value.length != 0) {
             setEmailError("Email must be at least 2 characters");
         }
-        else{
+        else {
             setEmailError("");
             setEmail(e.target.value);
         }
     }
-    const passwordHandle = (e)=>{
-        if(e.target.value.length<8&&e.target.value.length!=0){
+    const passwordHandle = (e) => {
+        if (e.target.value.length < 8 && e.target.value.length != 0) {
             setPasswordError("Password must be at least 8 characters");
         }
-        else{
+        else {
             setPasswordError("");
             setPassword(e.target.value);
         }
     }
-    const confirmPasswordHandle = (e)=>{
-        if(e.target.value!=password&&e.target.value.length!=0){
+    const confirmPasswordHandle = (e) => {
+        if (e.target.value != password && e.target.value.length != 0) {
             setConfPasswordError("Confirm Password must match password");
         }
-        else{
+        else {
             setConfPasswordError("");
             setPasswordConf(e.target.value);
         }
@@ -66,34 +69,40 @@ const Form = () => {
                     <label htmlFor={"firstName"}>First Name: </label>
                     <input type={"text"} onChange={fNameHandle}></input>
                     <div>
-                        <p style={{color:"red"}}>{firstNameError}</p>
+                        <p style={{ color: "red" }}>{firstNameError}</p>
                     </div>
                 </div>
                 <div>
                     <label htmlFor={"lastName"}>Last Name: </label>
                     <input type={"text"} onChange={lNameHandle}></input>
                     <div>
-                        <p style={{color:"red"}}>{lastNameError}</p>
+                        <p style={{ color: "red" }}>{lastNameError}</p>
                     </div>
                 </div>
                 <div>
                     <label htmlFor={"email"}>Email: </label>
                     <input type={"email"} onChange={emailHandle}></input>
                     <div>
-                        <p style={{color:"red"}}>{emailError}</p>
+                        <p style={{ color: "red" }}>{emailError}</p>
                     </div>
                 </div>
                 <div>
                     <label htmlFor={"password"}>Password: </label>
                     <input name={'password'} type={"password"} onChange={passwordHandle}></input>
                     <div>
-                        <p style={{color:"red"}}>{passwordError}</p>
-                        <p style={{color:"red"}}>{confPasswordError}</p>
+                        <p style={{ color: "red" }}>{passwordError}</p>
+                        <p style={{ color: "red" }}>{confPasswordError}</p>
                     </div>
                 </div>
                 <div>
                     <label htmlFor={"passwordconf"}>Confirm Password: </label>
                     <input name={'passwordconf'} type={"password"} onChange={confirmPasswordHandle}></input>
+                </div>
+                <div>
+                    <label htmlFor={"city"}>City: </label>
+                    <select name={'city'} onChange={e => setCity(e.target.value)}>
+                        {cities.map((item, i) => <option key={i} value={item}>{item}</option>)}
+                    </select>
                 </div>
             </form>
             <hr></hr>
@@ -102,6 +111,7 @@ const Form = () => {
             <p>Email: <span>{email}</span></p>
             <p>Password: <span>{password}</span></p>
             <p>Confirm Password: <span>{passwordConf}</span></p>
+            <p>City: <span>{city}</span></p>
         </div>
     )
 }
